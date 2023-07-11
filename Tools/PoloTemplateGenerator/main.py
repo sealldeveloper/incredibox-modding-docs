df2b052202551649b85004b0eb46aec7a4acc92c
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-07-12 00:58:25 +1000

    padding fix, better watermark

--- Tools/PoloTemplateGenerator/main.py
+++ Tools/PoloTemplateGenerator/main.py
@@ -13,7 +13,7 @@ if os.path.exists(f'{name}.txt'):
 nearest5=5*math.ceil(headscount/5)
 rows=nearest5/5
 col=nearest5/rows
-template=Image.new('RGBA',(int(width*col),int(height+17+(rows*headHeight))), (255, 255, 255, 0))
+template=Image.new('RGBA',(int(width*col),int(height+(rows*headHeight))), (255, 255, 255, 0))
 draw=ImageDraw.Draw(template)
 draw.rectangle((0,0,width,height), fill=(255,0,0))
 draw.text((width/2-width/10,height/2-height/20),"Default",fill=(0,0,0))
@@ -21,7 +21,7 @@ draw.rectangle((width,0,width*2,height), fill=(0,255,0))
 draw.text((width+width/2-width/10,height/2-height/20),"Headless",fill=(0,0,0))
 draw.rectangle((width*2,0,width*3,height), fill=(0,0,255))
 draw.text((2*width+width/2-width/10,height/2-height/20),"Siloette",fill=(0,0,0))
-draw.text((0,0),f"sealldeveloper's Template Generator: {name},{totalframe},{height},{width},{headHeight},{headscount}",fill=(0,0,0))
+draw.text((0,0),f"sealldeveloper's Template Generator\n{name},{totalframe},{height},{width},{headHeight},{headscount}",fill=(0,0,0))
 
 heads=Image.new('RGBA',(int(width*col),int((rows*headHeight))), (255, 255, 255, 0))
 headswidth=width*col
@@ -42,7 +42,7 @@ for i in range(0,int(rows)):
             with open(f'{name}_coordinates.txt','a') as f:
                 f.write(f'Head {textcount} Coords: {j*width},{i*headHeight+height}\n')
 
-template.paste(heads,(0,height+17),heads)
+template.paste(heads,(0,height),heads)
 template.save(f'{name}_template.png')
 
 arrayFrame=[]
