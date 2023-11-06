73a83967382fedf31e688b0125403c25e7b2827a
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-11-07 05:26:14 +1100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 termux-change-repo
-pkg install git python3 -y
+pkg install git python tur-repo python-numpy -y
 termux-setup-storage
-cd storage/shared/Documents/
+read -p "Press ENTER once you've accepted the storage access for termux!" < /dev/tty
+cd storage/shared/Documents
 git clone https://github.com/sealldeveloper/incredibox-modding-docs
-cd storage/shared/Documents/incredibox-modding-docs/
+cd incredibox-modding-docs
+pkg install openssl -y
