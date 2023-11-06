62e927be896eb2f5021fd2fbb5b2d17033256743
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-11-07 05:03:33 +1100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+termux-change-repo
+pkg install git python3 -y
+termux-setup-storage
 cd storage/shared/Documents/
 git clone https://github.com/sealldeveloper/incredibox-modding-docs
+cd storage/shared/Documents/incredibox-modding-docs/
