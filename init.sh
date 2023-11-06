5a850d71428ea31af064308e88cfdd5d7fd28b31
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-11-07 05:45:23 +1100

    added run files and updates install termux sccript

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 termux-change-repo
-pkg install git python tur-repo python-numpy -y
+apt update
+pkg install git python tur-repo build-essential cmake ninja libandroid-execinfo python-numpy libcrypt zlib python-pillow -y
 termux-setup-storage
 read -p "Press ENTER once you've accepted the storage access for termux!" < /dev/tty
 cd storage/shared/Documents
 git clone https://github.com/sealldeveloper/incredibox-modding-docs
 cd incredibox-modding-docs
+apt full-upgrade
+python3 -m pip install setuptools wheel packaging pyproject_metaddata cython
